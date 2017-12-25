adb939fc9bfd270f65759c860ec3e19f3afed4ac
Benjamin Atkin <ben@benatkin.com>
2017-12-24 20:31:43 -0800

    added matrix incoming webhook example

---
+++ matrix-incoming-webhook/index.js
@@ -0,0 +1,43 @@
+// 1. Go to [riot.im][riot] and sign up for an account
+// 2. Create a new channel
+// 3. Invite `@_webhook:t2bot.io` to your room, or your own webhook user
+//    if you're running your own matrix server and webhook bridge. See
+//    [the webhook bridge][webhook-bridge] docs. Wait for Webhook Bridge
+//    to join (it will no longer appear as "invited" in the sidebar).
+//    This may take a while, or you may need to run your own Matrix
+//    server and webhook bridge.
+// 4. Send the message `!webhook` to your room
+// 5. Wait a little while for Webhook Bridge to send you a message. Try
+//    sending `!webhook` again.
+// 6. When you get a message, grab the token at the end of the URL and
+//    set the environment variable: `export WEBHOOK_TOKEN=<token>`
+//
+// [riot]: https://riot.im/
+// [webhook-bridge]: https://github.com/turt2live/matrix-appservice-webhooks
+const axios = require('axios');
+
+class Client {
+  constructor({token, displayName, avatarUrl}) {
+    Object.assign(this, {token, displayName, avatarUrl});
+  }
+
+  async sendMessage(message) {
+    await axios.post(`https://webhooks.t2bot.io/api/v1/matrix/hook/${this.token}`, {
+      text: message,
+      format: 'plain',
+      displayName: this.displayName,
+      avatarUrl: this.avatarUrl
+    });
+  }
+}
+
+async function start() {
+  const client = new Client({
+    token: process.env.WEBHOOK_TOKEN,
+    displayName: 'example-bot',
+    avatarUrl: 'https://robohash.org/example-bot'
+  });
+  await client.sendMessage('[matrix] rocks!');
+}
+
+start().catch(err => { console.error(err); });
